8040af1ad30a5731582324696a87483261f9907b
RocqJones <jonesmbindyo@gmail.com>
2021-03-10 23:11:42 +0300

    Message Constructor

---
+++ app/src/main/java/com/intoverflown/friendlychatkt_android/FriendlyMessage.kt
@@ -0,0 +1,15 @@
+package com.intoverflown.friendlychatkt_android
+
+class FriendlyMessage {
+    var text: String? = null
+    var name: String? = null
+    var photoUrl: String? = null
+
+    constructor() {}
+
+    constructor(text: String?, name: String?, photoUrl: String?) {
+        this.text = text
+        this.name = name
+        this.photoUrl = photoUrl
+    }
+}
