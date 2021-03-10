05c515d4b5dc8c7184cecf9055bfdb1b34f0e7f5
RocqJones <jonesmbindyo@gmail.com>
2021-03-10 23:12:02 +0300

    Glide Module

---
+++ app/src/main/java/com/intoverflown/friendlychatkt_android/FriendlyChatGlideModule.kt
@@ -0,0 +1,7 @@
+package com.intoverflown.friendlychatkt_android
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class FriendlyChatGlideModule : AppGlideModule()// Intentionally empty.
