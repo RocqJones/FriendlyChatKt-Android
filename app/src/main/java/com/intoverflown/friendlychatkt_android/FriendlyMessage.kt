bfe21556e65995349045513e69386696924192d0
RocqJones <jonesmbindyo@gmail.com>
2021-03-12 11:06:31 +0300

    initialized empty constructor

--- app/src/main/java/com/intoverflown/friendlychatkt_android/FriendlyMessage.kt
+++ app/src/main/java/com/intoverflown/friendlychatkt_android/FriendlyMessage.kt
@@ -5,6 +5,8 @@ class FriendlyMessage {
     var name: String? = null
     var photoUrl: String? = null
 
+    constructor() {}
+
     constructor(text: String?, name: String?, photoUrl: String?) {
         this.text = text
         this.name = name
